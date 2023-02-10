b09768866f43fd477eb5719462bd8f8cba3a5952
Anahit Kasamanyan <anikakasamanyan@gmail.com>
2023-02-10 21:40:59 +0100

    yes

--- cpp01/ex00/Zombie.h
+++ cpp01/ex00/Zombie.h
@@ -2,11 +2,11 @@
 #define ZOMBIE_H
 # include "Zombie.hpp"
 
-/* It creates a zombie, name it, and return 
-	it so you can use it outside of the function */
-Zombie*	newZombie( std::string name );
 /* It creates a zombie, name it, and the
 	zombie announces itself. */
-void	randomChump( std::string name );
+void	randomChump(std::string name);
+/* It creates a zombie, name it, and return 
+	it so you can use it outside of the function */
+Zombie	*newZombie(std::string name);
 
 #endif
