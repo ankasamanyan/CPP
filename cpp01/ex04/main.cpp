1d6e9e86ac02700492014cc705d47e31e925047e
Anahit K <anikakasamanyan@gmail.com>
2023-02-20 19:02:10 +0100

    da

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -7,14 +7,11 @@ void	printMsg(std::string stringy, std::string colour)
 	std::cout << std::endl << colour << "+" << std::string(49, '=') << "+" << RESET_LINE;
 	std::cout << RESET << (stringy.size() > 16 ? "	" : "		");
 	std::cout<< "..." << stringy << "..." << RESET_LINE;
-	std::cout << colour << "+" << std::string(49, '=') << "+" << RESET_LINE;
+	std::cout << colour << "+" << std::string(49, '=') << "+" << std::endl << RESET_LINE;
 }
 
 bool	wrongInput(int argc, char **argv, std::ifstream	&file)
 {
-	(void)argv;
-	(void)file;
-	// (void)newFile;
 	if (argc != 4)
 	{
 		printMsg("Wrong number of arguments", PINK);
@@ -33,14 +30,27 @@ int main(int argc, char  *argv[])
 {
 	std::ifstream	file;
 	std::ofstream	newFile;
+	std::string		temp;
+	std::string		fileContent;
 
-	
 	if (wrongInput(argc, argv, file))
 		return (0);
 	newFile.open(std::string(argv[1]).append(".replace"));
+	while(std::getline(file, temp))
+		fileContent.append(temp + "\n");
+	if (!fileContent.empty())
+		fileContent.erase(fileContent.end() - 1);
+	newFile << fileContent;
+	
+	// for (size_t i = 0; i < fileContent.find(argv[2]); i++)
+	// {
 
+	// }
+	
+	// while (fileContent.find(argv[2]))
+	// {
 
-	// std::cout << std::string (argv[1]).append(".replace") << std::endl;
+	// }
 
 	return (0);
 }
