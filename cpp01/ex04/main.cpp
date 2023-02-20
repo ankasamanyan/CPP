4f1554002e2c6c4b5b2fbba1be3c8da299d60632
Anahit K <anikakasamanyan@gmail.com>
2023-02-20 20:03:32 +0100

    replace single accurance halppp

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -32,6 +32,7 @@ int main(int argc, char  *argv[])
 	std::ofstream	newFile;
 	std::string		temp;
 	std::string		fileContent;
+	// std::string::iterator	iter_;
 
 	if (wrongInput(argc, argv, file))
 		return (0);
@@ -40,8 +41,14 @@ int main(int argc, char  *argv[])
 		fileContent.append(temp + "\n");
 	if (!fileContent.empty())
 		fileContent.erase(fileContent.end() - 1);
+	size_t	index = fileContent.find(argv[2]);  //index of the first occurance
+	fileContent.erase(fileContent.begin() + index, fileContent.begin() + index + std::string(argv[2]).length());
+	// fileContent.erase(fileContent.begin() + index, fileContent.begin() + index + (std::string(argv[2])).length);
+	// std::cout << "begin: " << index << std::endl << "end: " << index + std::string(argv[2]).length() << std::endl;
+	// (fileContent.begin() + index)
+	fileContent.insert(index, std::string(argv[3]));
 	newFile << fileContent;
-	
+
 	// for (size_t i = 0; i < fileContent.find(argv[2]); i++)
 	// {
 
