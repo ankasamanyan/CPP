3a174d822e2b89db2532f73bb8a6db86b3a38a0e
Anahit K <anikakasamanyan@gmail.com>
2023-02-20 13:47:21 +0100

    errorssss

---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,43 @@
+#include "Colours.h"
+#include <iostream>
+#include <fstream>
+
+void	printMsg(std::string stringy, std::string colour)
+{
+	std::cout << std::endl << colour << "+=================================================+" << RESET_LINE;
+	std::cout << RESET << (stringy.size() > 16 ? "	" : "		");
+	std::cout<< "..." << stringy << "..." << RESET_LINE;
+	std::cout << colour << "+=================================================+" << std::endl << RESET_LINE;
+}
+
+bool	wrongInput(int argc, char **argv, std::ifstream	&file/* , std::ofstream &newFile */)
+{
+	(void)argv;
+	(void)file;
+	// (void)newFile;
+	if (argc != 4)
+	{
+		printMsg("Wrong number of arguments", PINK);
+		return (true);
+	}
+	file.open(argv[1]);
+	if (!file)
+	{
+		printMsg("No such file or directory", YELLOW);
+		return (true);
+	}
+	newFile = file.
+	return (false);	
+}
+
+int main(int argc, char  *argv[])
+{
+	std::ifstream	file;
+	std::ofstream	newFile;
+	
+	if (wrongInput(argc, argv, file, newFile))
+		return (0);
+	// std::ifstream file = argv[1];
+
+	return (0);
+}
