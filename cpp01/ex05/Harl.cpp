11dd3ac744ef0a9026132fc5e897bec36ab7cc7c
Anahit K <anikakasamanyan@gmail.com>
2023-02-21 14:44:14 +0100

    Harl thing

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -69,8 +69,8 @@ void    Harl::complain( std::string level )
 
 void	printMsg(std::string stringy, std::string colour)
 {
-	std::cout << std::endl << colour << "+" << std::string(49, '=') << "+" << RESET_LINE;
+	std::cout << std::endl << colour << "+" << std::string(50, '=') << "+" << RESET_LINE;
 	std::cout << RESET << (stringy.size() > 16 ? "	" : "		");
 	std::cout<< "..." << stringy << "..." << RESET_LINE;
-	std::cout << colour << "+" << std::string(49, '=') << "+" << std::endl << RESET_LINE;
+	std::cout << colour << "+" << std::string(50, '=') << "+" << std::endl << RESET_LINE;
 }
