ca5cc0e0ec68d44b46ee0f9d2c68aa40270dc1ad
Anahit K <anikakasamanyan@gmail.com>
2023-02-24 15:58:52 +0100

    kinda works

---
+++ cpp02/Fixed.cpp
@@ -0,0 +1,37 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	_raw = 0;
+	std::cout << GREEN << "Default constructor called" << RESET_LINE;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << PURPLE << "Destructor called" << RESET_LINE;
+}
+
+Fixed &Fixed::operator=(Fixed &copy)
+{
+    _raw = copy._raw;
+    std::cout << YELLOW << "Copy constructor called" << RESET_LINE;
+	return (*this);
+}
+
+ Fixed::Fixed(Fixed &copy)
+ {
+    *this = copy;
+    std::cout << PINK << "Copy assignment operator called" << RESET_LINE;
+ }
+
+int Fixed::getRawBits( void ) const
+{
+	std::cout << SKY << "getRawBits member function called" << RESET_LINE;
+	return (_raw);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	_raw = raw;
+	std::cout << "Halp" << RESET_LINE;
+}
