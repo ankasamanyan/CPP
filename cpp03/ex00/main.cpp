f34d67de59c21bac2d74f7d03cbed828e0247675
Anahit K <anikakasamanyan@gmail.com>
2023-03-03 13:49:56 +0100

    ✨

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -2,27 +2,37 @@
 
 int main()
 {
-	ClapTrap	trap("Cedrik");
+	/* test 1 */
+	ClapTrap catto("Catto");
+    ClapTrap doggo("Doggo");
 
-	trap.attack("Anaaaaaaaaaa");
-	trap.beRepaired(2);
-	trap.takeDamage(5);
-	trap.attack("Anaaaaaaaaaa");
-	trap.attack("Anaaaaaaaaaa");
-	trap.attack("Anaaaaaaaaaa");
-	trap.attack("Anaaaaaaaaaa");
-	trap.attack("Anaaaaaaaaaa");
-	trap.attack("Anaaaaaaaaaa");
-	trap.attack("Anaaaaaaaaaa");
-	trap.attack("Anaaaaaaaaaa");
-	trap.attack("Anaaaaaaaaaa");
-	trap.attack("Anaaaaaaaaaa");
-	trap.takeDamage(20);
-	trap.beRepaired(5);
+    catto.attack("Mouse");
+    catto.beRepaired(2);
+    doggo.attack("Birdy");
+    doggo = catto;
+    doggo.attack("Birdy");
+    catto = doggo;
+    doggo.takeDamage(5);
+    doggo.beRepaired(2);
 
-	// std::cout << trap._name << std::endl;
-	// std::cout << trap._attackDamage << std::endl;
-	// std::cout << trap._energyPoints << std::endl;
-	// std::cout << trap._hitPoints << std::endl;
-	return 0;
+	/* test 2 */
+	// ClapTrap	trap("Cedrik");
+
+	// trap.attack("Anaaaaaaaaaa");
+	// trap.beRepaired(2);
+	// trap.takeDamage(5);
+	// trap.attack("Anaaaaaaaaaa");
+	// trap.attack("Anaaaaaaaaaa");
+	// trap.attack("Anaaaaaaaaaa");
+	// trap.attack("Anaaaaaaaaaa");
+	// trap.attack("Anaaaaaaaaaa");
+	// trap.attack("Anaaaaaaaaaa");
+	// trap.attack("Anaaaaaaaaaa");
+	// trap.attack("Anaaaaaaaaaa");
+	// trap.attack("Anaaaaaaaaaa");
+	// trap.attack("Anaaaaaaaaaa");
+	// trap.takeDamage(20);
+	// trap.beRepaired(5);
+
+	return (0);
 }
