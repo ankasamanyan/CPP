59c2a97eb6c4f06d6831d18bc9d2a36d419895e8
Anahit K <anikakasamanyan@gmail.com>
2023-03-03 18:53:01 +0100

    ✨

--- cpp03/ex02/FragTrap.hpp
+++ cpp03/ex02/FragTrap.hpp
@@ -18,5 +18,4 @@ class FragTrap: public ClapTrap
 };
 
 
-
 #endif
--- cpp03/ex02/main.cpp
+++ cpp03/ex02/main.cpp
@@ -2,9 +2,9 @@
 
 int main()
 {
-	// ScavTrap	trap("Cederiques");
 	FragTrap	trap("Cederiques");
 	trap.attack("Halp");
+	trap.highFivesGuys();
 	trap.beRepaired(3);
 	return 0;
 }
