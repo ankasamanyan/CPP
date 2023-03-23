37ca8946374f2ebc2407067e052da8d8659c3e3f
Anahit K <anikakasamanyan@gmail.com>
2023-03-23 16:37:13 +0100

    da

---
+++ cpp05/ex02/Bureaucrat.hpp
@@ -0,0 +1,40 @@
+#ifndef BUREAUCRAT_HPP
+	#define BUREAUCRAT_HPP
+#include <iostream>
+#include "Colours.h"
+
+class Form;
+class Bureaucrat
+{
+	private:
+		const std::string	_name;
+		int					_grade;
+	public:
+		Bureaucrat();
+		Bureaucrat(const std::string &name, int	grade);
+		Bureaucrat(const Bureaucrat &copy);
+		Bureaucrat &operator=(const Bureaucrat &copy);
+		~Bureaucrat();
+		/* member functions */
+		const std::string	getNAme();
+		int					getGrade();
+		void				setGrade(int grade);
+		void				increaseGrade();
+		void				decreaseGrade();
+		void				signForm(Form &form);
+		/* exceptions */
+		class	GradeTooHighException: public std::exception
+		{
+			public:
+				const char *what() const throw();
+		};
+		class	GradeTooLowException: public std::exception
+		{
+			public:
+				const char *what() const throw();
+		};
+};
+
+std::ostream &operator<<(std::ostream &stream, Bureaucrat &bur);
+
+#endif
