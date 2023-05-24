7731739f590c6d05cc273d7e4c9ccc6b03d53bb7
Anahit K <anikakasamanyan@gmail.com>
2023-05-24 19:56:28 +0200

    yey✨

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -19,112 +19,166 @@ ScalarConverter::~ScalarConverter()
 {
 }
 
-static bool	isJustChar(std::string str)
+/* Helper functions */
+
+static int findChar(std::string input, char c)
 {
-	std::string::const_iterator it = str.begin();
-	if (isascii(*it))
-		return (true);
-	return (false);
+	int amount = 0;
+	for (std::string::const_iterator it = input.begin(); it != input.end(); ++it)
+   		if (*it == c)
+      		amount++;
+	return (amount);
 }
 
-static bool findChar(std::string input, char c)
+static int noGarbageInDouble(int c)
 {
-	for (std::string::const_iterator it = input.begin(); it != input.end(); ++it)
-	{
-   		if (*it == c)
-      		return (true);
-  	}
-	return (false);
+	if (!isnumber(c) && c != '.')
+		return (false);
+	return (true);
 }
+
+static int noGarbageInFloat(int c)
+{
+	if (!isnumber(c) && c != '.' && c != 'f')
+		return (false);
+	return (true);
+}
+
+static bool strIter(std::string stringy, int(*func)(int c))
+{
+	for (std::string::const_iterator it = stringy.begin(); it != stringy.end(); ++it)
+	{
+		if (!(*func)(*it))
+			return(false);
+	}
+	return(true);
+}
+
+/* Pseudo literals */
 
 static void ifPseudo(std::string input)
 {
 	if (input.compare("nan") == 0 || input.compare("+inf") == 0 || input.compare("-inf") == 0)
 	{
-		std::cout << PURPLE << "char:	" << GREEN << "impossible" << RESET_LINE;
-		std::cout << PURPLE << "int:	" << GREEN << "impossible" << RESET_LINE;
-		std::cout << PURPLE << "float:	" << GREEN << input << "f" << RESET_LINE;
-		std::cout << PURPLE << "char:	" << GREEN << input << RESET_LINE;
+		std::cout << PURPLE << "char:    " << GREEN << "impossible" << RESET_LINE;
+		std::cout << PURPLE << "int:     " << GREEN << "impossible" << RESET_LINE;
+		std::cout << PURPLE << "float:    " << GREEN << input << "f" << RESET_LINE;
+		std::cout << PURPLE << "double:   " << GREEN << input << RESET_LINE;
 		exit (0);
 	}
 }
 
-static bool inputIsValid(std::string input)
-{
-	int	dot = 0;
-	unsigned long size = 0;
+/* Detecting the types */
 
-	for (std::string::const_iterator it = input.begin(); it != input.end(); ++it)
-		if (*it == '.')
-			dot++;
-	for (std::string::const_iterator it = input.begin(); it != input.end(); ++it)
-	   	if (isnumber(*it) || (input[input.size() - 1] == 'f' || (*it == '.' && dot < 2 
-			&& input[input.size() - 1] != '.' )))
-			size++;
-	if ((findChar(input, 'f') && !findChar(input, '.') ) || input[0] == '.' || input[0] == '0' )
-		return (false);
-	if (input.size() > 2 && (!isnumber(input[input.size() - 1]) && !isnumber(input[input.size() - 2])))
-		return (false);
-	if (size  == input.size() && input.size() != 0)
+static bool	detectChar(std::string input)
+{
+	if (input.size() == 1)
+	{
+		if (isascii(input[0]) && !isnumber(input[0]))
+			return (true);
+	}
+	return (false);
+}
+
+static bool detectInt(std::string input)
+{
+	if(input[0] == '-' || input[0] == '+')
+		input = input.substr(1);
+	if (strIter(input, isnumber))
 		return (true);
 	return (false);
 }
 
-static void	charCast(std::string input, double value)
+static bool detectFloat(std::string input)
 {
-	if (isJustChar(input))
+	if(input[0] == '-' || input[0] == '+')
+		input = input.substr(1);
+	if ((findChar(input,'.') == 1 && (findChar(input,'f') == 1 && input[input.size()-1] == 'f') && strIter(input, noGarbageInFloat)))
 	{
-		if (value > 32 && value <= 126 )
-			std::cout << PURPLE << "char:	" << GREEN << "'" << static_cast<char>(value) << "'" << RESET_LINE;
-		else if (input.size() == 1 && isprint(value))
-			std::cout << PURPLE << "char:	" << GREEN << "'" << input << "'" << RESET_LINE;
-		else if (!isprint(value))
-			std::cout << PURPLE << "char:	" << GREEN << "Non displayable" << RESET_LINE;
-		else
-			std::cout << PURPLE << "char:	" << GREEN << "impossible" << RESET_LINE;
+		if ((input[0] != '.' && input[input.size() - 1] != '.') && input[input.find('.') + 1] != 'f')
+			return (true);
 	}
+	return(false);
 }
 
-static void	intCast(double value)
+static bool	detectDouble(std::string input)
 {
-	if (value >= INT_MIN && value <= INT_MAX)
+	if(input[0] == '-' || input[0] == '+')
+		input = input.substr(1);
+	if (findChar(input,'.') == 1 && strIter(input, noGarbageInDouble))
 	{
-		std::cout << PURPLE << "int:	" << GREEN << static_cast<int>(value) << RESET_LINE;
+		if (input[0] != '.' && input[input.size() - 1] != '.')
+			return (true);
 	}
+	return (false);
+}
+
+/* Validating the input type */
+
+static int isValidType(std::string input)
+{
+	if (input.size() == 0)
+		return (INVALID);
+	if (detectChar(input))
+		return (CHAR);
+	if (detectInt(input))
+		return (INT);
+	if (detectFloat(input))
+		return (FLOAT);
+	if (detectDouble(input))
+		return (DOUBLE);
+	return (INVALID);
+}
+
+/* Type casting */
+
+static void	charCast(std::string input, double value)
+{
+	if (isValidType(input) == CHAR) 
+		std::cout << PURPLE << "char:    " << GREEN << "'" << static_cast<char>(input[0]) << "'" << RESET_LINE;
+	else if (value > 32 && value <= 126)
+			std::cout << PURPLE << "char:    " << GREEN << "'" << static_cast<char>(value) << "'" << RESET_LINE;
 	else
-		std::cout << PURPLE << "int:	" << GREEN << "impossible" << RESET_LINE;
+		std::cout << PURPLE << "char:    " << GREEN << "non displayable" << RESET_LINE;
+}
+
+static void	intCast(std::string input, double value)
+{
+	if (detectChar(input) == CHAR)
+		std::cout << PURPLE << "int:      " << GREEN << static_cast<int>(input[0]) << RESET_LINE;
+	else if (value >= INT_MIN && value <= INT_MAX)
+		std::cout << PURPLE << "int:      " << GREEN << value << RESET_LINE;
+	else
+		std::cout << PURPLE << "int:      " << GREEN << "impossible" << RESET_LINE;
 }
 
 static void	floatCast(std::string input, double value)
 {
-	if ((value >= INT_MIN && value <= INT_MAX) && inputIsValid(input))
-	{
-		if (findChar(input, '.') && findChar(input, 'f'))
-			std::cout << PURPLE << "float:	" << GREEN << input << RESET_LINE;
-		else if (findChar(input, '.') && !findChar(input, 'f'))
-			std::cout << PURPLE << "float:	" << GREEN << input << "f" << RESET_LINE;
-		else if (!findChar(input, '.') && !findChar(input, 'f'))
-			std::cout << PURPLE << "float:	" << GREEN << value << ".0f" << RESET_LINE;
 
-	}
+	if (isValidType(input) == FLOAT)
+		std::cout << PURPLE << "float:    " << GREEN << input << RESET_LINE;
+	else if (isValidType(input) == DOUBLE)
+		std::cout << PURPLE << "float:    " << GREEN << input << "f" << RESET_LINE;
+	else if (isValidType(input) == INT)
+		std::cout << PURPLE << "float:    " << GREEN << value << ".0f" << RESET_LINE;
+	else if (isValidType(input) == CHAR)
+		std::cout << PURPLE << "float:    " << GREEN << static_cast<int>(input[0]) << ".0f" << RESET_LINE;
 	else
-		std::cout << PURPLE << "float:	" << GREEN << "impossible" << RESET_LINE;
+		std::cout << PURPLE << "float:    " << GREEN << "impossible" << RESET_LINE;
 }
 
 static void	doubleCast(std::string input, double value)
 {
-	if (inputIsValid(input) && (value >= INT_MIN && value <= INT_MAX))
-	{
-		if (findChar(input, '.') && !findChar(input, 'f'))
-			std::cout << PURPLE << "double:	" << GREEN << input << RESET_LINE;
-		else if (findChar(input, 'f'))
-			std::cout << PURPLE << "double:	" << GREEN << input.substr(0, input.size() - 1) << RESET_LINE;
-		else
-			std::cout << PURPLE << "double:	" << GREEN << input << ".0" << RESET_LINE;
-	}
+	if (isValidType(input) == INT)
+		std::cout << PURPLE << "double:   " << GREEN << value << ".0" << RESET_LINE;
+	else if (isValidType(input) == DOUBLE)
+		std::cout << PURPLE << "double:   " << GREEN << input << RESET_LINE;
+	else if (isValidType(input) == FLOAT)
+		std::cout << PURPLE << "double:   " << GREEN << input.substr(0, input.size() - 1) << RESET_LINE;
+	else if (isValidType(input) == CHAR)
+		std::cout << PURPLE << "double:   " << GREEN << static_cast<int>(input[0]) << ".0" << RESET_LINE;
 	else
-		std::cout << PURPLE << "double:	" << GREEN << "impossible" << RESET_LINE;
-
+		std::cout << PURPLE << "double:   " << GREEN << "impossible" << RESET_LINE;
 }
 
 void	ScalarConverter::convert(std::string input)
@@ -133,11 +187,11 @@ void	ScalarConverter::convert(std::string input)
 	double	value;
 
 	ifPseudo(input);
-	if (!inputIsValid(input))
+	if (isValidType(input) == INVALID)
 		return(printMsg("INVALID INPUT", PINK));
 	value = strtod(input.c_str(), &stringy);
 	charCast(input, value);
-	intCast(value);
+	intCast(input, value);
 	floatCast(input, value);
 	doubleCast(input, value);
 
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -5,6 +5,12 @@
 #include <cstdlib>
 #include <cmath>
 
+#define INVALID		0
+#define CHAR		1
+#define INT			2
+#define FLOAT		3
+#define DOUBLE		4
+
 class ScalarConverter
 {
 	private:
