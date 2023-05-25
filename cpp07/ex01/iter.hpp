9b7af1e7abbdcd6631bb731f41d050e053221aca
Anahit K <anikakasamanyan@gmail.com>
2023-05-25 14:41:16 +0200

    iter✨

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -1,10 +1,31 @@
 #ifndef ITER_HPP
 	#define ITER_HPP
+#include <iostream>
+#include "Colours.h"
+
 template <typename T>
 void	iter(T	*array, int size, void(*func)(T &elem))
 {
 	for (int i = 0; i < size; i++)
-		(*func)array[i];
+		(*func)(array[i]);
+}
+
+/* Print functionf for strings (for testing) */
+void	printStringy(std::string	&stringy)
+{
+	std::cout << PURPLE << ">> " << GREEN << stringy << RESET_LINE;
+}
+
+/* Print functionf for int (for testing) */
+void	printInt(int	&i)
+{
+	std::cout << PURPLE << ">> " << YELLOW << i << RESET_LINE;
+}
+
+/* Increase functionf for int (for testing) */
+void	increasetInt(int	&i)
+{
+	std::cout << PURPLE << "++ " << SKY << ++i << RESET_LINE;
 }
 
 #endif
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -2,6 +2,17 @@
 
 int main()
 {
-	/* code */
+	int	intArray[] = { 1, 42, 3, 4, 5, 6, 7, 89, 4215, 3456};
+	std::string strArray[] = {"Do not go gentle into that good night,", 
+							"Old age should burn and rave at close of day;",
+							"Rage, rage against the dying of the light.",
+							"Though wise men at their end know dark is right,",
+							"Because their words had forked no lightning they",
+							"Do not go gentle into that good night."};
+	iter(strArray, 6, printStringy);
+	std::cout << RESET_LINE << std::string (50, '-') << "\n"<< RESET_LINE;
+	iter(intArray, 10, printInt);
+	std::cout << RESET_LINE << std::string (50, '-') << "\n"<< RESET_LINE;
+	iter(intArray, 10, increasetInt);
 	return 0;
 }
