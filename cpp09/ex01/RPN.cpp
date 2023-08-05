8b0b9152d38b280617f1c1542174330262a9550c
Anahit K <anikakasamanyan@gmail.com>
2023-08-05 18:32:40 +0200

    ✨

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -1,22 +1,5 @@
 #include "RPN.hpp"
 
-// You must create a program with these constraints:
-
-// • The program name is RPN.
-
-// • Your program must take an inverted Polish mathematical expression as an argument.
-
-// • The numbers used in this operation and passed as arguments will always be less 
-// than 10. The calculation itself but also the result do not take into account this rule.
-
-// • Your program must process this expression and output the correct result on the 
-// standard output.
-
-// • If an error occurs during the execution of the program an error message 
-// should be displayed on the standard output.
-
-// • Your program must be able to handle operations with these tokens: "+ - / *".
-
 bool	is_sign(char element)
 {
 	if (element == '+' || element == '-' || element == '/' || element == '*')
